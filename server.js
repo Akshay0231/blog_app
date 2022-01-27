5602d0b7f7c0b862a5cb0b931606b6357bdb16e0
Akshay0231 <akshaay2301@gmail.com>
2022-01-27 18:26:53 +0530

    MongoDB Connection Sertup

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config()
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: true,
+}).then(console.log("Connected to MongoDB Server"))
+    .catch((err) => console.log(err))
 
 // MIDDLEWARE
 app.use(express.json())
