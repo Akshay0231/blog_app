7c00233f53835786f1dbefb0222e06e99eedbef5
Akshay0231 <akshaay2301@gmail.com>
2022-01-27 18:44:20 +0530

    Post Model defined

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+        required: false
+    },
+    username: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamp: true
+})
+
+module.exports = mongoose.model('Post', postSchema)
